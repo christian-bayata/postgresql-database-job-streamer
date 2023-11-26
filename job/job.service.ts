81776dd31e8beabda2265b370bc7f5ef17459d61
christian-bayata <franksagie1@gmail.com>
2023-11-26 22:00:56 +0100

    Complete implementation of job

--- job/job.service.ts
+++ job/job.service.ts
@@ -7,14 +7,14 @@ import { Client } from 'pg';
 export class JobService {
   constructor(private readonly configService: ConfigService) {}
 
-  private logger = new Logger('Main');
+  private logger = new Logger('Job');
   /**
    * @Responsibility: Cron job that runs every day by 12am to stream database
    *
    * @returns {}
    */
 
-  //@Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async databaseStreamer() {
     try {
       /* Connect to both the staging and local databases */
@@ -23,7 +23,7 @@ export class JobService {
         new Client(this.databaseConfig('local')).connect(),
       ]);
 
-      /* Retrieve all table rows form the staging database */
+      /* Retrieve all table rows from the staging database */
       const allStagingDbTableNames = await stagingDbClient
         .query(
           'SELECT table_name FROM information_schema.tables WHERE table_schema = $1',
@@ -36,38 +36,21 @@ export class JobService {
       for (let i = 0; i < theLength; i++) {
         const tableName = allStagingDbTableNames[i];
 
-        const theStagingQuery = stagingDbClient.query(
-          `SELECT * FROM ${tableName}`,
-        );
-
-        /* Drop the existing collection in the local db */
-        const theLocalCollection = localDbClient.db().collection(tableName);
-
-        /* Ensure the local database has a collection before streaming into it */
-        const localCollectionExists = await localDbClient
-          .db()
-          .listCollections({ name: tableName })
-          .hasNext();
-
-        if (!localCollectionExists) {
-          await localDbClient.db().createCollection(tableName);
-          this.logger.log(
-            `Added collection to the local database: ${tableName}`,
-          );
-        }
+        const theStagingQuery = `SELECT * FROM ${tableName}`;
+        const theLocalQuery = `INSERT INTO ${tableName} SELECT * FROM ${tableName}`;
 
         /* Drop local database for fresh streaming */
-        await theLocalCollection.drop();
+        await localDbClient.query(`DELETE FROM ${tableName}`);
 
         /* Move data from staging to local collection in streams */
-        const stream = theStagingQuery.find().stream();
+        const stream = stagingDbClient.query(theStagingQuery).stream();
 
         stream.on('data', async (doc) => {
-          await theLocalCollection.insertOne(doc);
+          await localDbClient.query(theLocalQuery);
         });
 
         stream.on('end', () => {
-          this.logger.log(`Data migration completed for ${collectionName}`);
+          this.logger.log(`Data migration completed for table ${tableName}`);
         });
 
         stream.on('error', (error) => {
@@ -78,8 +61,8 @@ export class JobService {
         await new Promise((resolve) => stream.on('end', resolve));
       }
 
-      stagingDbClient.close();
-      localDbClient.close();
+      await stagingDbClient.end();
+      await localDbClient.end();
     } catch (error) {
       throw error;
     }
